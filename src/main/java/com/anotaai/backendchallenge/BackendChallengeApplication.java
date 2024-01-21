9518256502e4856c0b45f41504951b9cfe9f8d83
Diego Baptista <diego.rbaptista@gmail.com>
2024-01-21 09:25:34 -0300

    initial commit

---
+++ src/main/java/com/anotaai/backendchallenge/BackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.anotaai.backendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengeApplication.class, args);
+	}
+
+}
