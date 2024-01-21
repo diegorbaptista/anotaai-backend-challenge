2a6aaac73bd16cd834480daaa934e7e2c1d9106c
Diego Baptista <diego.rbaptista@gmail.com>
2024-01-21 09:29:33 -0300

    includes a category to a product domain model

--- src/main/java/com/anotaai/backendchallenge/domain/Product.java
+++ src/main/java/com/anotaai/backendchallenge/domain/Product.java
@@ -17,8 +17,9 @@ public class Product {
     private String description;
     private String ownerId;
     private Integer price;
+    private Category category;
 
-    public void modify(String title, String description, Integer price) {
+    public void modify(String title, String description, Integer price, Category category) {
         if (title != null && !title.isBlank()) {
             this.title = title;
         }
@@ -27,6 +28,9 @@ public class Product {
         }
         if (price > 0) {
             this.price = price;
+        }
+        if (category != null) {
+            this.category = category;
         }
     }
 
